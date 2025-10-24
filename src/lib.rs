abbf919a1d177daca685f2fff32c7f8f5dd1e4ff
ugrek <romanvetrin@gmail.com>
2025-10-24 16:03:45 +0200

    [+] made prelude

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,10 @@ pub mod schemas;
 pub mod ya;
 
 // Re-export key rig components that users of this crate will need
-pub use rig::agent::AgentBuilder;
-pub use rig::client::completion::CompletionModelHandle;
-pub use rig::client::*;
-pub use rig::prelude::*;
+pub mod prelude
+{
+  pub use crate::ya;
+  pub use rig::agent::AgentBuilder;
+  pub use rig::client::completion::CompletionModelHandle;
+  pub use rig::client::*;
+}
