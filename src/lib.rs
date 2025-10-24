6508d2704da31a9fa5596e8729a55e89243171ec
ugrek <romanvetrin@gmail.com>
2025-10-24 15:23:30 +0200

    [+] minor fix

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ pub mod schemas;
 pub mod ya;
 
 // Re-export key rig components that users of this crate will need
-pub use rig::client::{ProviderClient, CompletionClient, VerifyClient};
+pub use rig::client::*;
 pub use rig::prelude::*;
