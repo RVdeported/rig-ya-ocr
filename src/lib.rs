6ffd04f5a0c2edb20398b46012ea826cdb85b596
ugrek <romanvetrin@gmail.com>
2025-10-24 15:42:16 +0200

    [+] minor fix

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,7 @@ pub mod schemas;
 pub mod ya;
 
 // Re-export key rig components that users of this crate will need
+pub use rig::agent::AgentBuilder;
+pub use rig::client::completion::CompletionModelHandle;
 pub use rig::client::*;
 pub use rig::prelude::*;
